28303746cedec7e6f9af3386d421d511df9bf587
jedens5000 <mrjonedens@gmail.com>
2023-01-18 19:29:24 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submite():
+  if request.method == "POST":
+    customer = request.form["customer"]
+    dealer = request.form["dealer"]
+    rating = request.form["rating"]
+    comments = request.form["comments"]
+    print(customer, dealer, rating, comments)
+    return render_template("success.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
